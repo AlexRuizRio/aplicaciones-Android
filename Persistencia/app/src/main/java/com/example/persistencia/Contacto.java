45afb2cd0bc9a3e2f0a2b052baaa4d263a6e0745
AlexRuizRio <alejandro.ruizrio@gmail.com>
2025-04-29 20:40:46 +0200

    Empiece del proyecto de persistencia con sqllite

---
+++ Persistencia/app/src/main/java/com/example/persistencia/Contacto.java
@@ -0,0 +1,47 @@
+package com.example.persistencia;
+
+public class Contacto {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private int imagenid;
+
+    public Contacto (int id, String nombre, String telefono, int imagenid) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.imagenid = imagenid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public int getImagenid() {
+        return imagenid;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setImagenid(int imagenid) {
+        this.imagenid = imagenid;
+    }
+}
