54e28b8212e83c2cae6b247d0f9cd459112e6e36
Alex Ruiz <140838623+AlexRuizRio@users.noreply.github.com>
2025-05-04 21:20:34 +0200

    Añado vistas y activitys

--- Persistencia/app/src/main/java/com/example/persistencia/MainActivity.java
+++ Persistencia/app/src/main/java/com/example/persistencia/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.persistencia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView listaContactos;
+    private ContactoAdapter contactoAdapter;
+    private ContactoDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ImageView btnadd = findViewById(R.id.guardar);
+        ImageView btnoff = findViewById(R.id.apagar);
+       btnadd.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AgregarContactoActivity.class);
+            startActivity(intent);
         });
+        btnoff.setOnClickListener(v -> {
+            finishAffinity();
+        });
+        /*
+        btnadd.setOnClickListener(v ->
+                Toast.makeText(this, "Pulsaste Guardar", Toast.LENGTH_SHORT).show()
+        );
+
+        btnoff.setOnClickListener(v ->
+                Toast.makeText(this, "Pulsaste Apagar", Toast.LENGTH_SHORT).show()
+        );
+        */
+        listaContactos = findViewById(R.id.listacontactos);
+        dbHelper = new ContactoDBHelper(this);
+
+        cargarContactos();
+
+        // Ejemplo: abre una actividad para agregar contacto
+        // findViewById(R.id.botonAgregar).setOnClickListener(v -> {
+        //     Intent intent = new Intent(this, AgregarContactoActivity.class);
+        //     startActivity(intent);
+        // });
+    }
+
+    private void cargarContactos() {
+        List<Contacto> contactos = dbHelper.obtenerContactos();
+        contactoAdapter = new ContactoAdapter(this, contactos);
+        listaContactos.setAdapter(contactoAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cargarContactos();  // Recarga por si se han añadido/actualizado contactos
     }
 }
